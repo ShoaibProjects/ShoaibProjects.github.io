dd964e08967ba82df9e47a83298c7986157d6221
ShoaibProjects <shoaibakhtarcs@gmail.com>
2024-01-15 01:36:51 +0530

    improvements to dark mode

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-let n=document.getElementById('name');
+let body=document.body;
+let moon=document.getElementById('imoon');
+let sun=document.getElementById('isun');
+
 function dark(){
-    let body=document.body;
-    body.classList.toggle("dark-mode");
+    body.classList.add("dark-mode");
+    moon.classList.remove("bx-moon");
+    moon.classList.add("bxs-moon");
+    sun.classList.remove("bxs-sun");
+    sun.classList.add("bx-sun");
+}
+function light(){
+    body.classList.remove("dark-mode");
+    moon.classList.add("bx-moon");
+    moon.classList.remove("bxs-moon");
+    sun.classList.add("bxs-sun");
+    sun.classList.remove("bx-sun");
 }
