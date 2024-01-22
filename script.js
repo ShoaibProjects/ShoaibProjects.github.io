724344040bb3a838c09670d28badecb88a5072d3
ShoaibProjects <shoaibakhtarcs@gmail.com>
2024-01-23 00:37:28 +0530

    dark mode in responsiveness

--- script.js
+++ script.js
@@ -19,6 +19,23 @@ function light(){
 function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".ham-icon");
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+let hmoon=document.getElementById('hmoon');
+let hsun=document.getElementById('hsun');
+
+function hdark(){
+    body.classList.add("dark-mode");
+    hmoon.classList.remove("bx-moon");
+    hmoon.classList.add("bxs-moon");
+    hsun.classList.remove("bxs-sun");
+    hsun.classList.add("bx-sun");
+}
+function hlight(){
+    body.classList.remove("dark-mode");
+    hmoon.classList.add("bx-moon");
+    hmoon.classList.remove("bxs-moon");
+    hsun.classList.add("bxs-sun");
+    hsun.classList.remove("bx-sun");
 }
